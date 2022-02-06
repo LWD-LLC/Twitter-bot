99ed8c9f9d59509ada17590f54aba2b24c9fb93c
LWD-LLC <87009143+LWD-LLC@users.noreply.github.com>
2022-02-06 15:46:55 -0500

    finished tutorial

--- functions/index.js
+++ functions/index.js
@@ -10,18 +10,73 @@ const twitterClient = new TwitterApi({
     clientSecret: 'Your_CLient_Secret',
 });
 
-// need for the callback on twitter http://127.0.0.1:5000/limitless-tw/us-central1/callback
+// need for the callback on twitter 
 
-const callbackURL = 'http://127.0.0.1:5000/limitless-tw/us-central1/callback'
+const callbackURL = 'YOUR CALLBACK URL'
 
 exports.auth = functions.https.onRequest((request, response) => {
+    const { url, codeVerifier, state } = twitterClient.generateOAuth2AuthLink(
+        callbackURL,
+        { scope: ['tweet.read', 'tweet.write', 'users.read', 'offline.access'] }
+    );
 
+    // store verifier
+    await dbRef.set({ codeVerifier, state });
+    response.redirect(url)
 })
 
 exports.callback = functions.https.onRequest((request, response) => {
-    
+    const { state, code } = request.query;
+
+    const dbSnapshot = await dbRef.get();
+    const { codeVerifier, state: storedState } = dbSnapshot.data();
+
+    if (state !== storedState) {
+        return response.status(400).send('Stored tokens do not match!');
+    }
+
+    const {
+        client: loggedClient,
+        accessToken,
+        refreshToken
+    } = await twitterClient.loginWithOAuth2({
+        code,
+        codeVerifier,
+        redirectUri: callbackURL
+    });
+
+    await dbRef.set({ accessToken, refreshToken });
+
+    response.sendStatus(200);
 })
 
 exports.tweet = functions.https.onRequest((request, response) => {
+    const { refreshToken } = (await dbRef.get()).data();
+
+    const { 
+        client: refreshedClient,
+        accessToken,
+        refreshToken: newRefreshToken,
+    } = await twitterClient.refreshOAuth2Token(refreshToken)
+
+    await dbRef.set({ accessToken, refreshToken: newRefreshToken })
     
+    const nextTweet = await openai.createCompletion('text-davinci-001', {
+        prompt: 'tweet something cool for #techtwitter',
+        max_tokens: 64,
+    });
+
+    const { data } = await refreshedClient.v2.tweet(
+        nextTweet.data.choices[0].text
+    )
+
+    response.send(data);
 })
+
+const { Configuration, OpenAIApi } = require('openai');
+const configuration = new Configuration({
+    organization: 'YOUR ORGANIZATION',
+    apiKey: 'YOUR API KEY'
+});
+
+const openai = new OpenAIApi(configuration);
